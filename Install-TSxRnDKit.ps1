a32e4e6c89ba41d7625d5a40ce71b556cf18985c
DeploymentBunny <deploymentbunny@outlook.com>
2019-01-09 17:57:23 +0100

    Update

--- Install-TSxRnDKit.ps1
+++ Install-TSxRnDKit.ps1
@@ -1,15 +1,5 @@
-﻿Param(
-    [string]$TSxPlasterTemplatePath
-)
-#Installer for TSxRnD
+﻿#Installer for TSxRnD
 Write-Host "Installing TSxRnD 1.0"
-
-#Creating Folder for PlasterTemplates
-New-Item -Path $TSxPlasterTemplatePath -ItemType Directory -ErrorAction Stop -Force
-
-#Adding RegValue for Template Path
-New-Item -Path HKCU:\TSx -Force
-New-ItemProperty -Path HKCU:\TSx -Name PlatserTemplatePath -PropertyType String -Value $TSxPlasterTemplatePath -Force -ErrorAction Stop
 
 #Install Module for Plaster
 Install-Module -Name Plaster -SkipPublisherCheck -ErrorAction Stop
