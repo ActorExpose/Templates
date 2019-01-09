17de9a9bb22d16723977c8c341ec46654e8f8c36
Anders Axhake <anders.axhake@truesec.se>
2019-01-09 19:21:36 +0100

    Changed TLS version for Invoke-Webrequest

--- TSxRnD.psm1
+++ TSxRnD.psm1
@@ -4,6 +4,7 @@ Function Update-TSxTemplates{
     #Download Template folder as ZIP from GitHub
     Import-Module -Name Plaster -ErrorAction Stop
     $TemplatePath = "$((Get-Module -Name Plaster).ModuleBase)\Templates"
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     Invoke-WebRequest "https://github.com/TrueSec-Infra/Templates/archive/master.zip" -OutFile $env:TEMP\TSxTemplates.zip
     If((Test-Path $env:TEMP\TSxTemplates.zip) -eq $true){
         Write-Host "Download was successful"
