51ded223da90967314373786620d85989b953ff5
DeploymentBunny <deploymentbunny@outlook.com>
2019-01-10 00:12:36 +0100

    Update

---
+++ SampleInstall.ps1
@@ -0,0 +1,19 @@
+﻿#Install Plaster, Pester and TSxRnD
+Invoke-WebRequest "https://raw.githubusercontent.com/TrueSec-Infra/Templates/master/Install-TSxRnDKit.ps1" -OutFile $env:TEMP\Install-TSxRnDKit.ps1
+& $env:TEMP\Install-TSxRnDKit.ps1
+
+#Set/update config for PowerShellscripts
+Import-Module TSxRnD -Force -Verbose
+Set-TSxDefaultUserData -Author "First Last" -Twitter "@Handle" -EMail "first.last@company.com" -Blog "https://blog.com" -Company "Company" -CompanyWeb "https://www.company.com"
+
+#Download the templates from our GitHub Repo
+Update-TSxTemplates
+
+#Create Scripts for Community
+New-TSxPowerShellScript -Path C:\Temp -ScriptName Test1.ps1 -TemplateName Default -Class Community
+
+#Create Scripts for Company
+New-TSxPowerShellScript -Path C:\Temp -ScriptName Test1.ps1 -TemplateName Default -Class Company
+
+#Get-PlasterTemplate
+Invoke-Plaster -TemplatePath "C:\Program Files\WindowsPowerShell\Modules\Plaster\1.1.3\Templates\NewPowerShellScriptModule" -DestinationPath C:\Temp -Force
--- TSxRnD.psm1
+++ TSxRnD.psm1
@@ -3,14 +3,23 @@
 Function Update-TSxTemplates{
     #Download Template folder as ZIP from GitHub
     Import-Module -Name Plaster -ErrorAction Stop
-    $TemplatePath = "$((Get-Module -Name Plaster).ModuleBase)\Templates"
+    
     Invoke-WebRequest -Uri "https://github.com/TrueSec-Infra/Templates/archive/master.zip" -OutFile $env:TEMP\TSxTemplates.zip
     If((Test-Path $env:TEMP\TSxTemplates.zip) -eq $true){
         Write-Host "Download was successful"
+
         Expand-Archive -Path $env:TEMP\TSxTemplates.zip -DestinationPath $env:TEMP -Force
-        $TemplateFolders = Get-ChildItem -Path $env:TEMP\templates-master -Directory
+
+        $TemplatePath = "$((Get-Module -Name Plaster).ModuleBase)\Templates"
+        $TemplateFolders = Get-ChildItem -Path $env:TEMP\templates-master\PlasterTemplates -Directory
         foreach($TemplateFolder in $TemplateFolders){
             Copy-Item -Path $TemplateFolder.FullName -Destination $TemplatePath -Recurse -Force
+        }
+
+        $TemplatePath = "$((Get-Module -Name TSxRnD).ModuleBase)\Templates"
+        $Templates = Get-ChildItem -Path $env:TEMP\templates-master\ScriptTemplates -Directory
+        foreach($Template in $Templates){
+            Copy-Item -Path $Template.FullName -Destination $TemplatePath -Recurse -Force
         }
         Remove-Item -Path $env:TEMP\TSxTemplates.zip
         Remove-Item -Path $env:TEMP\templates-master -Recurse
