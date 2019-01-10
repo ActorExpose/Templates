cc46c5d117f55fc4a039f225e1aa925a01ff1349
DeploymentBunny <deploymentbunny@outlook.com>
2019-01-10 01:00:27 +0100

    Update of a sample param

--- SampleInstall.ps1
+++ SampleInstall.ps1
@@ -13,7 +13,7 @@ Update-TSxTemplates
 New-TSxPowerShellScript -Path C:\Temp -ScriptName Test1.ps1 -TemplateName Default -Class Community
 
 #Create Scripts for Company
-New-TSxPowerShellScript -Path C:\Temp -ScriptName Test1.ps1 -TemplateName Default -Class Company
+New-TSxPowerShellScript -Path C:\Temp -ScriptName Test2.ps1 -TemplateName Default -Class Company
 
 #Get-PlasterTemplate
 Invoke-Plaster -TemplatePath "C:\Program Files\WindowsPowerShell\Modules\Plaster\1.1.3\Templates\NewPowerShellScriptModule" -DestinationPath C:\Temp -Force
